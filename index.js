b1e13cce4e502cdc455bee4d98a637c4e1e37544
Kevin Kelley <kkelley622@gmail.com>
2021-12-30 10:26:35 -0600

    commit tree 142c9a15ff51aa9077a94134d1c4146bd43cdc92

--- index.js
+++ index.js
@@ -13,6 +13,24 @@
 
 //wrapGifts(gifts);
 
+//for (let age = 30; age < 40; age++) {
+    //console.log(`I'm ${age} years old. Happy birthday to me!`);
+    
+//}
+
+//const gifts = ["teddy bear", "drone", "doll"];
+
+//function wrapGifts(gifts) {
+  //for (let i = 0; i < gifts.length; i++) {
+    //console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    
+  //}
+
+  //return gifts;
+//}
+
+//wrapGifts(gifts);
+
 function writeCards(names, event) {
     let messages = [];
     for(let i = 0; i < names.length; i++) {
@@ -28,3 +46,4 @@ function countDown(positiveNumber) {
         positiveNumber--;
     }
 }
+
